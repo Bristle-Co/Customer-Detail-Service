2b57cd57cb254fba7f569bf9a9ce25910ea6b17d
Anderson Hsieh <andy13579246810@gmail.com>
2022-07-18 01:02:29 -0400

    add more data into data.sql, also fix bug in entity converter

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,4 +9,47 @@ INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_
 VALUES ('U22-測試', '測試', '測試', '測試', '測試', '測試', '測試', '測試', '測試', '測試', '測試');
 INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
                        address, tax_id, receiver, note)
-VALUES ('U59-隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+VALUES ('U59-隨機1', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+
+INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
+                       address, tax_id, receiver, note)
+VALUES ('U59-隨機2', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
+                       address, tax_id, receiver, note)
+VALUES ('U59-隨機3', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
+                       address, tax_id, receiver, note)
+VALUES ('U59-隨機4', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
+                       address, tax_id, receiver, note)
+VALUES ('U59-隨機5', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
+                       address, tax_id, receiver, note)
+VALUES ('U59-隨機6', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
+                       address, tax_id, receiver, note)
+VALUES ('U59-隨機7', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
+                       address, tax_id, receiver, note)
+VALUES ('U59-隨機8', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
+                       address, tax_id, receiver, note)
+VALUES ('U59-隨機9', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
+                       address, tax_id, receiver, note)
+VALUES ('U59-隨機10', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
+                       address, tax_id, receiver, note)
+VALUES ('U59-隨機11', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
+                       address, tax_id, receiver, note)
+VALUES ('U59-隨機12', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
+                       address, tax_id, receiver, note)
+VALUES ('U59-隨機13', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
+                       address, tax_id, receiver, note)
+VALUES ('U59-隨機14', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
+INSERT INTO customers (customer_id, name, contact_name, contact_number, contact_mobile_number, fax_number, postal_code,
+                       address, tax_id, receiver, note)
+VALUES ('U59-隨機15', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機', '隨機');
